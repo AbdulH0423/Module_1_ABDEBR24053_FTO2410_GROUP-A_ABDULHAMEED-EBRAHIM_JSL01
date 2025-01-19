0ffe022c56c40a4c5e29befd7cddaac0cd93eccd
AbdulH0423 <zain.ebrahim8@gmail.com>
2025-01-19 14:31:56 +0200

    Added code to check if string is valid or not
    added code to check if input string is valid or invalid

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ function validateSyntax() {
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
 
+        if(regex.test(input)){
+            result = "Valid syntax";
+        }
+        else{
+            result = "invalid syntax.";
+        }
+
             document.getElementById('result').innerText = result;
 }
 
